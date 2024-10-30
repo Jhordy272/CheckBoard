3df8558460270a0a38c4fa88f2241ee224925b4a
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-10-30 18:11:27 -0500

    Create the entity for JobTitle

---
+++ src/main/java/com/check_board/security/entity/JobTitle.java
@@ -0,0 +1,29 @@
+package com.check_board.security.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "job_title")
+public class JobTitle {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "hourly_rate")
+    private int hourlyRate;
+}
