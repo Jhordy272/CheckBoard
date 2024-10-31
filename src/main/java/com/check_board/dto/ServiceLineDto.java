c69b345df1061eac113f10072b75e263abf093ae
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-10-30 19:07:12 -0500

    Create ServiceLineDto

---
+++ src/main/java/com/check_board/dto/ServiceLineDto.java
@@ -0,0 +1,13 @@
+package com.check_board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceLineDto {
+    private Integer id;
+    private String name;
+}
